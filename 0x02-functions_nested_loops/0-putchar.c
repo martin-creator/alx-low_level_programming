43b765721d71b72daff5692d6d7d31de59aaee4e
martin-creator <martinlubowa@outlook.com>
2022-03-15 21:29:51 +0300

    put char

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,19 @@
-#include <main.h>
-
+#include "main.h"
 /**
-  *A function  that prints _putchar
-*/
-
+ * main - prints _putchar
+ * Return: 0
+ */
 int main(void)
 {
-        printf("_putchar")
+	char putchar[] = "_putchar";
+	int i = 0;
+
+	while (putchar[i] != '\0')
+	{
+		_putchar(putchar[i]);
+		i++;
+	}
+	_putchar('\n');
+
 	return (0);
 }
