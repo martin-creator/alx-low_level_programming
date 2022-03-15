23bac8215e1c5e1a45590d873c17ee83dbcacc9f
martin-creator <martinlubowa@outlook.com>
2022-03-15 20:59:53 +0300

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+
+/* betty style doc for function main goes there */
+/**
+  *A function  that prints _putchar
+*/
+
+int main(void)
+{
+        printf("_putchar")
+	return (0);
+}
