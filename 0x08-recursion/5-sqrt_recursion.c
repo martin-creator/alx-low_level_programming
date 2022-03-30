1730e28f4615d3923750363129006d7496074dc8
martin-creator <53592131+martin-creator@users.noreply.github.com>
2022-03-30 16:46:39 +0300

    Updates

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,37 +1,44 @@
 #include "main.h"
-#include <stdio.h>
-
-int square(int n, int s);
-
 /**
- * square is the base function
- * _sqrt_recursion - get the natural square root of a number
- * @n: is the natural number
- * Return: The natural square root
- * function that returns the natural square root of a number.
+ *_evaluate - evaluate function sqrt
+ *@i: interger
+ *@n: interger
+ *Return: evaluate sqrt
  */
 
-int _sqrt_recursion(int n)
+int _evaluate(int i, int n)
 {
-return (square(n, 0));
+/*Evaluate function*/
+if (n == 0 || n == 1)
+return (n);
+
+else if (i * i < n)
+return (_evaluate(i + 1, n));
+
+else if (i * i == n) /*Condiction base*/
+return (i);
+
+return (-1);
+
+return (-1);
 }
 
 /**
- * @s: is a counter
- * Return: The sqrt of a natural number
- * If n does not have a natural square root, the function should return -1
- */
+ *_sqrt_recursion - evaluate sqrt
+ *@n: interger
+ *Return: Sgrt_recursion
+*/
 
-int square(int n, int s)
+int _sqrt_recursion(int n)
 {
+int i = 0;
 
-if (s * s == n)
-return (s);
-
-else if (s * s > n)
+if (i < 0) /*If n is negative*/
 return (-1);
 
 else
-return (square(n, s + 1));
+{
+return (_evaluate(i, n)); /*Recursive call*/
+}
 
 }
