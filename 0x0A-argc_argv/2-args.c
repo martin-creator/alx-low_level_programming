16d0875d5aa8b7ca4256c50f5f422e761de8f9e1
martin-creator <martinlubowa@outlook.com>
2022-04-01 16:37:45 +0300

    a program that prints all arguments it receives

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints all arguments it receives
+ * @argc: number of all command line arguments
+ * @argv: array containing all command line arguments
+ *Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	
+	for (i=0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
