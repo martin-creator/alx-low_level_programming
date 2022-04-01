66b23ebd809b0a49ff36aa82d369501344eb21d0
martin-creator <martinlubowa@outlook.com>
2022-04-01 16:15:32 +0300

    a program that prints its name, followed by a new line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *main - prints program name , followed by  a new line
+ * @argc : int
+ * @argv : list
+ * @return : 0
+ */
+
+int main(int argc, char const *argv[])
+{
+(void)argc;
+printf("%s", argv[0]);
+return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void _putchar(char);
+int main(int argc, char const *argv[]);
+
+#endif /* MAIN_H */
+
