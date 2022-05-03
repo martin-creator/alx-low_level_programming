49582ea7ad1c1decb63cc7f7a8adebad5c0f0235
martin-creator <martinlubowa@outlook.com>
2022-05-03 04:17:07 +0300

    a function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,49 +2,37 @@
 
 /**
  * create_file - creates a file
- * @filename: filename
- * @text_content: content written in the file
+ * @filename: filename.
+ * @text_content: content writed in the file.
  *
- * Return: returns either 1 on success or -1 on failure
+ * Return: return either 1 success or -1 on failure.
  */
-
 int create_file(const char *filename, char *text_content)
 {
+	int ftp;
+	int numberLetters;
+	int writtenLetters;
 
-int ftp;
-int numLetters;
-int writtenLetters;
+	if (!filename)
+		return (-1);
 
-if (!filename)
-{
-	return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (ftp == -1)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	for (numberLetters = 0; text_content[numberLetters]; numberLetters++)
+		;
+
+	writtenLetters = write(ftp, text_content, numberLetters);
+
+	if (writtenLetters == -1)
+		return (-1);
+
+	close(ftp);
+
+	return (1);
 }
-
-ftp = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
-if (ftp == -1)
-{
-	return (-1);
-}
-
-if (!text_content)
-{
-	return (-1);
-}
-
-for (numLetters = 0; text_content[numLetters]; numLetters++)
-	;
-
-writtenLetters = write(ftp, text_content, numLetters);
-
-if (writtenLetters == -1)
-{
-	return (-1);
-}
-
-close(ftp);
-
-return (1);
-
-}
-
