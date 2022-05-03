8854fa421eeb80960e015cb0ec162760034eb1ae
martin-creator <martinlubowa@outlook.com>
2022-05-03 04:19:12 +0300

    a function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,34 +5,34 @@
  * @filename: filename.
  * @text_content: content writed in the file.
  *
- * Return: return either 1 success or -1 on failure.
+ * Return: 1 if it success. -1 if it fails.
  */
 int create_file(const char *filename, char *text_content)
 {
-	int ftp;
-	int numberLetters;
-	int writtenLetters;
+	int fd;
+	int nletters;
+	int rwr;
 
 	if (!filename)
 		return (-1);
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (ftp == -1)
+	if (fd == -1)
 		return (-1);
 
 	if (!text_content)
 		text_content = "";
 
-	for (numberLetters = 0; text_content[numberLetters]; numberLetters++)
+	for (nletters = 0; text_content[nletters]; nletters++)
 		;
 
-	writtenLetters = write(ftp, text_content, numberLetters);
+	rwr = write(fd, text_content, nletters);
 
-	if (writtenLetters == -1)
+	if (rwr == -1)
 		return (-1);
 
-	close(ftp);
+	close(fd);
 
 	return (1);
 }
