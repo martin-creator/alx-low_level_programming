76aa0bf6ff5f4d33bf602dc1932ee27cabe7bec5
martin-creator <martinlubowa@outlook.com>
2022-07-01 17:22:14 +0300

    a function that gives you the index of a key

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -7,7 +7,6 @@
  * @key: key passed
  * @size: size of the hash tables
  * Return: key index
- * Note: Dividing by size spreads key value over available key space 
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
